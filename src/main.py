fbea4277483a1915de37600e12e42c6792a8c5c8
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-04-21 16:26:26 +0300

    file viewer with demo data

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import sys
 import supervisely_lib as sly
-
+import ui
 
 app: sly.AppService = sly.AppService()
 app_sources_dir = str(Path(sys.argv[0]).parents[1])
@@ -12,23 +12,31 @@ team_id = int(os.environ['context.teamId'])
 workspace_id = int(os.environ['context.workspaceId'])
 
 
-#@ui.handle_exceptions(app.task_id, app.public_api)
 @app.callback("preview")
 @sly.timeit
 def preview(api: sly.Api, task_id, context, state, app_logger):
-    pass
+    def filter(path):
+        return False if "/venv" in path else True
 
+    files = sly.fs.list_files_recursively(app_sources_dir, filter_fn=filter)
 
-def test():
-    pass
+    tree_items = []
+    for file in files:
+        tree_items.append({
+            "path": file,
+            "size": 1234
+        })
+    fields = [
+        {"field": "data.tree", "payload": tree_items},
+    ]
+    api.task.set_fields(task_id, fields)
 
 
 def main():
-    #test()
-    #exit(0)
-
     data = {}
     state = {}
+
+    ui.init_bucket_preview(data, state)
 
     app.run(data=data, state=state)
 
--- src/ui.py
+++ src/ui.py
@@ -1,2 +1,7 @@
 import supervisely_lib as sly
 
+
+def init_bucket_preview(data, state):
+    state["bucketName"] = ""
+    state["selected"] = ""
+    data["tree"] = None
